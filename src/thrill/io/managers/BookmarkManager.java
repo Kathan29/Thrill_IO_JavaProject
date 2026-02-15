a1f44dcae6bbcd1c59e078bbb67df4cc49e47592
Kathan Modi <kathanmodi2001@gmail.com>
2026-02-15 17:38:07 +0530

    Adding bookmark manager or services that create movies,create books and create weblink

---
+++ src/thrill/io/managers/BookmarkManager.java
@@ -0,0 +1,63 @@
+package thrill.io.managers;
+
+import thrill.io.entities.Book;
+import thrill.io.entities.Movie;
+import thrill.io.entities.Weblink;
+
+public class BookmarkManager {
+
+	public static BookmarkManager instance = new BookmarkManager();
+
+	private BookmarkManager() {
+
+	}
+
+	public static BookmarkManager getInstance() {
+		return instance;
+	}
+
+	public Movie createMovie(long id, String title, String profileUrl, int releaseYear, String[] cast,
+			String[] directors, String genre, double imdbRating) {
+		
+		Movie movie = new Movie();
+		
+		movie.setId(id);
+		movie.setTitle(title);
+		movie.setProfileUrl(profileUrl);
+		movie.setReleaseYear(releaseYear);
+		movie.setCast(cast);
+		movie.setDirectors(directors);
+		movie.setGenre(genre);
+		movie.setImdbRating(imdbRating);
+
+		return movie;
+	}
+
+	public Book createBook(long id, String title, int publicationYear, String publisher, String genre, String[] authors,
+			double amazonRating) {
+		
+		Book book = new Book();
+		
+		book.setId(id);
+		book.setTitle(title);
+		book.setPublicationYear(publicationYear);
+		book.setGenre(genre);
+		book.setAuthors(authors);
+		book.setAmazonRating(amazonRating);
+		book.setPublisher(publisher);
+		
+		return book;
+	}
+
+	public Weblink createWeblink(long id, String title, String url, String host) {
+		
+		Weblink weblink = new Weblink();
+		
+		weblink.setId(id);
+		weblink.setTitle(title);
+		weblink.setUrl(url);
+		weblink.setHost(host);
+		
+		return weblink;
+	}
+}
