b7c6f1c6e262b30c2696397280f7bf11a4babc2e
Kathan Modi <kathanmodi2001@gmail.com>
2026-02-18 12:31:30 +0530

    Updated browse method to add functionality of sharing also if user is editor or chiefEditor

--- src/thrillio/View.java
+++ src/thrillio/View.java
@@ -5,6 +5,7 @@ import thrillio.constants.UserType;
 import thrillio.controllers.BookmarkController;
 import thrillio.entities.Bookmark;
 import thrillio.entities.User;
+import thrillio.partners.Sharable;
 
 public class View {
 
@@ -14,28 +15,39 @@ public class View {
 		int bookmarkCount = 0;
 		for (Bookmark[] bookmarkList : bookmarks) {
 			for (Bookmark bookmark : bookmarkList) {
-					
-				//Check it user want to bookmark
+
+				// Check it user want to bookmark
 				if (bookmarkCount < DataStore.BOOKMARK_LIMIT) {
 					boolean toBookmark = (Math.random() < 0.5) ? true : false;
-					if(toBookmark) {
+					if (toBookmark) {
 						BookmarkController.getInstance().storeBookmarking(user, bookmark);
 						System.out.println("Item bookmarked : " + bookmark);
 						bookmarkCount++;
 					}
 				}
-				
-				//Mark it as kid friendly
-				if(user.getUserType().equals(UserType.CHIEF_EDITOR) || user.getUserType().equals(UserType.EDITOR)) {
-					if(bookmark.isKidFriendly() && bookmark.getKidFriendlyStatus().equals(KidFriendlyStatus.UNKNOWN)) {
+
+				// Do editor or chief editor task
+				if (user.getUserType().equals(UserType.CHIEF_EDITOR) || user.getUserType().equals(UserType.EDITOR)) {
+
+					// Approve or rejecting kid friendly status
+					if (bookmark.isKidFriendly() && bookmark.getKidFriendlyStatus().equals(KidFriendlyStatus.UNKNOWN)) {
 						String takeDecision = getKidFriendlyStatusDecision();
-						if(!takeDecision.equals(KidFriendlyStatus.UNKNOWN)) {
-							bookmark.setKidFriendlyStatus(takeDecision);
-							System.out.println("Kid Friendly Status : "+takeDecision+" ,"+bookmark);
-						}	
+						if (!takeDecision.equals(KidFriendlyStatus.UNKNOWN)) {
+
+							BookmarkController.getInstance().setKidFriendlyStatus(user,takeDecision, bookmark);
+						}
+					}
+					
+					
+					//Sharing the book or weblink to partners
+					if(bookmark.getKidFriendlyStatus().equals(KidFriendlyStatus.APPROVED) && bookmark instanceof Sharable) {
+						boolean shareDecision = (Math.random() < 0.5) ? true : false;
+						if(shareDecision) {
+							BookmarkController.getInstance().share(user,bookmark);
+						}
 					}
 				}
-				
+
 			}
 		}
 
@@ -43,7 +55,8 @@ public class View {
 
 	private static String getKidFriendlyStatusDecision() {
 		double val = Math.random();
-		return (val<0.4) ? KidFriendlyStatus.APPROVED : (val<0.8) ? KidFriendlyStatus.REJECTED : KidFriendlyStatus.UNKNOWN;
+		return (val < 0.4) ? KidFriendlyStatus.APPROVED
+				: (val < 0.8) ? KidFriendlyStatus.REJECTED : KidFriendlyStatus.UNKNOWN;
 	}
 
 }
